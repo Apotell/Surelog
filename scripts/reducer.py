4531e54a497571fe0cfcbe7e0ab086c44cba22f0
HS <hs@apotell.com>
2026-01-31 13:55:05 -0800

    Fix path vs. str issue when generating tarball

--- scripts/reducer.py
+++ scripts/reducer.py
@@ -321,7 +321,7 @@ def _run_one(params):
   rmfile(reduction_log_filepath)
 
   if is_ci_build():
-    generate_tarball(str(test_dirpath))
+    generate_tarball(test_dirpath)
     rmdir(str(test_dirpath))
 
   log(f'... {name} Completed.' if completed else f'... {name} FAILED.')
